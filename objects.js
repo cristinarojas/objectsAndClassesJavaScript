a9f9bd79cb9af59f9ee50b905dc21edd5b82b7cb
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-09-28 10:00:55 -0500

    Adding 3 ways to create an object in JS

--- objects.js
+++ objects.js
@@ -5,3 +5,22 @@ var myFirstObject = new Object(); // with new Object()
 var mySecondObject = {}; // Initializing to {}
 
 function constructorObject() {}; // Constructor function or class builder
+/* 
+	Con esta forma creamos una función que nos servirá
+	en el futuro para que un objeto se instancie a partir de esta ,es decir,
+	necesitaremos la palabra clave new 
+	para crear el objeto basado en esta función. 
+*/
+
+// Example: 
+
+// Instanciamos y creamos el objeto car
+function car(model, color, gas) {
+	this.model = model;
+	this.color = color;
+}
+
+// Referenciado a myCar
+var myCar = new car('nissan', 'red');
+
+console.log('myCar', myCar);
