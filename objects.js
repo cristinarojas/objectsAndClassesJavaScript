86e75fa9bdee6b342b2b04b2c38348ce5c7d44ac
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-09-28 09:53:26 -0500

    Adding 3 ways to create an object in JS

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
-// 3 Ways of declaring Objects in JS
+/* 3 Ways of declaring Objects in JS */
+
 var myFirstObject = new Object(); // with new Object()
 
 var mySecondObject = {}; // Initializing to {}
 
-function constructorObject() {}; // Constructor function
+function constructorObject() {}; // Constructor function or class builder
